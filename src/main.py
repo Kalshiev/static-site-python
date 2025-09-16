df91bdcfe74adc817b51a39c6be25ba007180fb9
Kalshiev <haescobedo@gmail.com>
2025-09-16 12:37:30 -0600

    Add initial project structure with main script, tests, and styles
    - Created main.sh to run the main application
    - Added .gitignore to exclude __pycache__ directory
    - Implemented main.py with a sample TextNode output
    - Added test_textnode.py for unit testing TextNode functionality
    - Created textnode.py to define TextNode and TextType classes
    - Added styles.css for basic styling
    - Created test.sh for running unit tests
    - Removed public/index.html as it was not needed

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode("This is some anchor text", "link", "https://bootdev.com"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+        node3 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node3)
+
+        node4 = TextNode("This is a text node", TextType.BOLD, url="http://example.com")
+        self.assertNotEqual(node, node4)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(repr(node), "TextNode(This is a text node, TextType.BOLD, None)")
+
+    def test_link_node(self):
+        node = TextNode("This is a link", TextType.LINK, url="http://example.com")
+        self.assertEqual(node.text, "This is a link")
+        self.assertEqual(node.text_type, TextType.LINK)
+        self.assertEqual(node.url, "http://example.com")
+        self.assertEqual(repr(node), "TextNode(This is a link, TextType.LINK, http://example.com)")
+    
+if __name__ == "__main__":
+    unittest.main()
