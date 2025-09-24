aa88293baccb3a2e57fb191b0eb5b0ac46dd043e
Kalshiev <haescobedo@gmail.com>
2025-09-24 15:06:19 -0600

    Refactor directory deletion and copying logic for improved clarity and functionality

--- src/copy_all_files.py
+++ src/copy_all_files.py
@@ -7,17 +7,23 @@ def delete_dir(dir):
         for file in files:
             print(f"deleting... {file}")
         shutil.rmtree(dir)
-    else:
-        print("Directory already deleted!")
-        print("Refreshing the directory")
-        os.mkdir("public")
+    os.mkdir(dir)
 
 def copy_dir(source, destination):
-    if os.path.exists(source):
-        files = os.listdir(source)
+    source_dir = os.path.abspath(source)
+    destination_dir = os.path.abspath(destination)
+    
+    if os.path.exists(source_dir):
+        files = os.listdir(source_dir)
         for file in files:
-            s_path = os.path.abspath(file)
-            print(s_path)
+            file_path = os.path.join(source_dir, file)
+            if os.path.isfile(file_path):
+                print(f"copying... {file_path}")
+                shutil.copy(file_path, destination_dir)
+            elif os.path.isdir(file_path):
+                new_destination = os.path.join(destination_dir, file)
+                os.mkdir(new_destination)
+                copy_dir(file_path, new_destination)
 
-
+delete_dir("./public")
 copy_dir("./static", "./public")
